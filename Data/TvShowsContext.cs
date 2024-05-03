c6f306b2d6b1dc6cbe1bbdaff1ab5e71d3091005
Adnan-hamid-wani <Adnanaduuu@gmail.com>
2024-05-03 23:30:04 +0530

    Add files via upload

---
+++ Data/TvShowsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project3.Models
+{
+    public class TvShowsContext : DbContext
+    {
+        public TvShowsContext (DbContextOptions<TvShowsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Project3.Models.TvShow> TvShow { get; set; } = default!;
+    }
+}
